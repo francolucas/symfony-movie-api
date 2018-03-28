e2058d8a0efdc07cfb2d982ec4137af6030e1ffe
Lucas Franco <lucas@lucasfranco.com.br>
2018-03-28 07:58:20 -0300

    Post validation with generic error message

--- application/src/AppBundle/Controller/MoviesController.php
+++ application/src/AppBundle/Controller/MoviesController.php
@@ -7,58 +7,63 @@ use FOS\RestBundle\Controller\ControllerTrait;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class MoviesController extends AbstractController
 {
-  use ControllerTrait;
+    use ControllerTrait;
 
-  /**
-   * @Rest\View()
-   */
-  public function getMoviesAction()
-  {
-    $movies = $this->getDoctrine()->getRepository('AppBundle:Movie')->findAll();
+    /**
+    * @Rest\View()
+    */
+    public function getMoviesAction()
+    {
+        $movies = $this->getDoctrine()->getRepository('AppBundle:Movie')->findAll();
 
-    return $movies;
-  }
-
-  /**
-   * @Rest\View(statusCode=201)
-   * @ParamConverter("movie", converter="fos_rest.request_body")
-   * @Rest\NoRoute()
-   */
-  public function postMoviesAction(Movie $movie)
-  {
-    $em = $this->getDoctrine()->getManager();
-    $em->persist($movie);
-    $em->flush();
-
-    return $movie;
-  }
-
-  /**
-   * @Rest\View()
-   */
-  public function deleteMovieAction(?Movie $movie)
-  {
-    if (null === $movie) {
-      return $this->view(null, 404);
+        return $movies;
     }
 
-    $em = $this->getDoctrine()->getManager();
-    $em->remove($movie);
-    $em->flush();
-  }
+    /**
+    * @Rest\View(statusCode=201)
+    * @ParamConverter("movie", converter="fos_rest.request_body")
+    * @Rest\NoRoute()
+    */
+    public function postMoviesAction(Movie $movie, ConstraintViolationListInterface $validationErrors)
+    {
+        if (count($validationErrors) > 0) {
+            throw new HttpException(400, 'The data is either invalid or incomplete');
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($movie);
+        $em->flush();
 
-  /**
-   * @Rest\View()
-   */
-  public function getMovieAction(?Movie $movie)
-  {
-    if (null === $movie) {
-      return $this->view(null, 404);
+        return $movie;
     }
 
-    return $movie;
-  }
+    /**
+    * @Rest\View()
+    */
+    public function deleteMovieAction(?Movie $movie)
+    {
+        if (null === $movie) {
+            return $this->view(null, 404);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($movie);
+        $em->flush();
+    }
+
+    /**
+    * @Rest\View()
+    */
+    public function getMovieAction(?Movie $movie)
+    {
+        if (null === $movie) {
+            return $this->view(null, 404);
+        }
+
+        return $movie;
+    }
 }
